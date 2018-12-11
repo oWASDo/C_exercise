94ae34edc6f668a3757cad238df130a94adbb49c
Simone <sem961@gmail.com>
2018-12-11 10:35:23 +0100

    Gfx implementation

---
+++ gfx.c
@@ -0,0 +1,24 @@
+#include "SDL.h"
+
+SDL_Texture load_texture(const char *file, SDL_Renderer *renderer)
+{
+    SDL_Surface *surface = SDL_LoadBMP(file);
+    if (surface == NULL)
+    {
+        SDL_Log(SDL_GetError());
+        return NULL;
+    }
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    if (texture == NULL)
+    {
+        SDL_Log(SDL_GetError());
+        return NULL;
+    }
+    return texture;
+}
+void render_image(SDL_Texture *texture, SDL_Renderer *renderer, const SDL_Rect s_rect, const SDL_Rect d_rect, const double angel, const SDL_Point center,const SDL_RendererFlip flip)
+{
+
+    SDL_RenderCopyEx(renderer, texture, s_rect, d_rect, angel, cente, flip);
+}
--- header.h
+++ header.h
@@ -1,1 +1,8 @@
-a
+#include "SDL.h"
+
+
+
+
+
+SDL_Texture load_texture(const char *file, SDL_Renderer *renderer);
+void render_image(SDL_Texture *texture, SDL_Renderer *renderer, const SDL_Rect s_rect, const SDL_Rect d_rect, const double angel, const SDL_Point center,const SDL_RendererFlip flip);
