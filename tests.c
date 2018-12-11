1fc4ae4261b68b11185daf99e1f83af4f8aaa7a1
Lorenzo Brunori <lorenzo.brunori97@gmail.com>
2018-12-11 12:38:56 +0100

    add second test that reset the timer

--- tests.c
+++ tests.c
@@ -7,20 +7,32 @@ TEST(clock_adding)
 {
 
     Uint32 last_tick = 1200;
-	printf("%i \n",last_tick);
+
     double rot = 0;
     int timer = 0;
 	Uint32 current_tick = 3600;
-	printf("%i \n",current_tick);
-	printf("%i \n", current_tick - last_tick);
+
 	clock_logic(current_tick, &rot, &timer, &last_tick);
 	ASSERT_THAT(rot > 0);
+}
+
+TEST(clock_reset)
+{
+
+    Uint32 last_tick = 1200;
+
+    double rot = 0;
+    int timer = 0;
+	Uint32 current_tick = 3600;
+
+	clock_logic(current_tick, &rot, &timer, &last_tick);
+	ASSERT_THAT(timer == 0);
 }
 
 int main(int argc, char **argv)
 {
 	RUN_TEST(clock_adding);
-
+	RUN_TEST(clock_reset);
 	PRINT_TEST_RESULTS();
 	return 0;
 }
